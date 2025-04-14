945d343ac9f48f5bb72e03f71c65f92db3bf8887
BPerryUML <146905848+BPerryUML@users.noreply.github.com>
2025-04-13 20:44:08 -0400

    Update Calculator.java
    Add Javadoc

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,12 +1,24 @@
 package com.leszko.calculator;
-
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
+/**
+ * This is the main file for the calculator source code
+ * @author Leszko -Update For Homework Feature test
+ * @version 1
+*/
+/**
+ * not sure what @service does EDIT for HOMEWORK
+*/
 @Service
+/** 
+ * The Calculator Class
+*/
 public class Calculator {
-        final static int umlNUMBER1 = 3;
-	@Cacheable("sum")
+/** 
+ * Adds together two numbers and returns the sum.
+ * @param a The first number to add
+ * @param b The second number to add
+*/
 	public int sum(int a, int b) {
 		return a + b;
 	}
