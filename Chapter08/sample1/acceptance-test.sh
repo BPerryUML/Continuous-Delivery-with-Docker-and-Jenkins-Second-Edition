686f99b5890cb7dbaef3edcc5c8747591275a323
BPerryUML <146905848+BPerryUML@users.noreply.github.com>
2025-04-14 05:44:34 -0400

    Update acceptance-test.sh

--- Chapter08/sample1/acceptance-test.sh
+++ Chapter08/sample1/acceptance-test.sh
@@ -2,5 +2,5 @@
 set -x
 
 NODE_IP=$(kubectl get service calculator-service --insecure-skip-tls-verify -o jsonpath='{.spec.clusterIP}')
-NODE_PORT=$(kubectl get service calculator-service --insecure-skip-tls-verify -o jsonpath='{.spec.nodePort}')
+NODE_PORT=$(kubectl get service calculator-service --insecure-skip-tls-verify -o jsonpath='{.spec.ports.targetPort}')
 ./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
