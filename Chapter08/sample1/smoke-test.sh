fc2b70ed1f410974a12545774e9ffa9e353be008
BPerryUML <146905848+BPerryUML@users.noreply.github.com>
2025-04-14 18:57:08 -0400

    Update smoke-test.sh

--- Chapter08/sample1/smoke-test.sh
+++ Chapter08/sample1/smoke-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-NODE_IP=$(kubectl get service calculator-service --insecure-skip-tls-verify -o jsonpath='{.spec.clusterIP}')
-NODE_PORT=$(kubectl get service calculator-service --insecure-skip-tls-verify -o jsonpath='{.spec.ports[0].targetPort}')
+NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[1].status.addresses[0].address}')
+NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].port)'
 ./gradlew smokeTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
